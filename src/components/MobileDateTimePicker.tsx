07e7ea7a430e173b6c99597b23715a01e4a04744
mo-nizar <getnizar@gmail.com>
2024-04-19 23:10:43 +0400

    resolved ts issues

--- src/components/MobileDateTimePicker.tsx
+++ src/components/MobileDateTimePicker.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { DesktopTimePicker } from '@mui/x-date-pickers/DesktopTimePicker';
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
-
 import dayjs from 'dayjs';
 import "@styles/components/DateTimeInput.scss";
 
@@ -14,69 +13,66 @@ interface BasicDateTimePickerProps {
   onChange: (newValue: Date | null) => void;
   label?: string;
   isRequired?: boolean;
-  className?: string,
+  className?: string;
 }
 
-const MobileDateTimePicker: React.FC<BasicDateTimePickerProps> = ({ value, isRequired, label, onChange, className }) => {
+interface DateTimeState {
+  time: dayjs.Dayjs | null;
+  date: dayjs.Dayjs | null;
+}
 
-  const [dateTime, setDateTime]= useState({time: null, date: null});
+const MobileDateTimePicker: React.FC<BasicDateTimePickerProps> = ({ value, onChange, label, className }) => {
+  const [dateTime, setDateTime] = useState<DateTimeState>({ time: null, date: null });
 
-  const onDateTimeChange=(value: any, isDate: boolean)=>{
-
-    if(isDate){
-      setDateTime({...dateTime, date: value})
-
-      if(dateTime.time){
-        
-        const dateObject = new Date(value.$d);
-        const timeObject = new Date(dateTime.time.$d);
-
+  const onDateTimeChange = (value: dayjs.Dayjs | null, isDate: boolean) => {
+    if (isDate) {
+      setDateTime({ ...dateTime, date: value });
+      if (dateTime.time && value) {
+        const dateObject = new Date(value.toDate());
+        const timeObject = new Date(dateTime.time.toDate());
         dateObject.setHours(timeObject.getHours());
         dateObject.setMinutes(timeObject.getMinutes());
         dateObject.setSeconds(timeObject.getSeconds());
         onChange(dateObject);
       }
-    }else{
-      setDateTime({...dateTime, time: value})
-
-      if(dateTime.date){
-        const dateObject = new Date(dateTime.date.$d);
-        const timeObject = new Date(value.$d);
-
+    } else {
+      setDateTime({ ...dateTime, time: value });
+      if (dateTime.date && value) {
+        const dateObject = new Date(dateTime.date.toDate());
+        const timeObject = new Date(value.toDate());
         dateObject.setHours(timeObject.getHours());
         dateObject.setMinutes(timeObject.getMinutes());
         dateObject.setSeconds(timeObject.getSeconds());
         onChange(dateObject);
       }
-    }      
-  }
+    }
+  };
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer components={['DateTimePicker']}>
         <div className='flex flex-row'>
-          <MobileDatePicker 
+          <MobileDatePicker
             className={`${className} container`}
             label={label || ''}
-            value={dateTime.date ?? null}
-            onChange={(val)=>onDateTimeChange(val, true)}
+            value={dateTime.date}
+            onChange={(val) => onDateTimeChange(val, true)}
             disablePast={true}
             closeOnSelect={true}
-
           />
           <DesktopTimePicker
             className={`${className} container`}
             label={label || ''}
-            value={dateTime.time ?? null}
-            onChange={(val)=>onDateTimeChange(val, false)}
-            ampm={false}     
-            timeSteps={{hours:1, minutes:30, seconds: 0}}
+            value={dateTime.time}
+            onChange={(val) => onDateTimeChange(val, false)}
+            ampm={false}
+            timeSteps={{ hours: 1, minutes: 30, seconds: 0 }}
             closeOnSelect={true}
-
           />
         </div>
       </DemoContainer>
     </LocalizationProvider>
   );
-}
+};
 
 export default MobileDateTimePicker;
